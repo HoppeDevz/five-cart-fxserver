560529e5fa3462705e75b57e1ccc0df3b61a4d9a
HoppeDevz <mrgamerbr1234@gmail.com>
2021-03-30 15:36:33 -0300

    change-http-to-https-in-config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     verify_timeout: 60 * 1000, // in ms
 
-    api_protocol: "http", // http or https
+    api_protocol: "https", // http or https
     client_domain: "minhaloja.fivecart.com.br",
     server_identifier: "identificador-do-servidor"
 }
